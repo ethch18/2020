cef35d9233375d3a861c1a6985d98dbbfc865618
Ethan Chau <echau18@cs.uw.edu>
2020-10-21 18:05:16 -0700

    preprocessed inputs

---
+++ task1/loanwords-data-cleaned/preprocess_tsv.py
@@ -0,0 +1,23 @@
+import argparse
+import hangul_jamo
+import re
+
+from tqdm import tqdm
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--input-file', type=str, required=True)
+parser.add_argument('--output-file', type=str, required=True)
+args = parser.parse_args()
+
+do_jamo = 'kor' in args.input_file.lower()
+re_diacritics = re.compile(r"[ː ᵝ̆  ͈  ̟ ̠ ̥ ̊ ̃ ̞ ˀ˕̹  ]")
+
+with open(args.input_file) as inf, open(args.output_file, 'w') as ouf:
+    for line in tqdm(inf):
+        grapheme, phoneme = line.strip().split('\t')
+        phoneme = re_diacritics.sub('', phoneme).replace('w͍', 'w')
+        if do_jamo:
+            grapheme = hangul_jamo.decompose(grapheme)
+
+        ouf.write(f"{grapheme}\t{phoneme}\n")
+
