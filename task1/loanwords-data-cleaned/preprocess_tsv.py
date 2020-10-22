c0e569763b8711bfcc0e4a4102108cba11b5704f
Ethan Chau <echau18@cs.uw.edu>
2020-10-21 19:12:35 -0700

    preprocess without removing spaces

--- task1/loanwords-data-cleaned/preprocess_tsv.py
+++ task1/loanwords-data-cleaned/preprocess_tsv.py
@@ -10,7 +10,9 @@ parser.add_argument('--output-file', type=str, required=True)
 args = parser.parse_args()
 
 do_jamo = 'kor' in args.input_file.lower()
-re_diacritics = re.compile(r"[ː ᵝ̆  ͈  ̟ ̠ ̥ ̊ ̃ ̞ ˀ˕̹  ]")
+# re_diacritics = re.compile(r"[ːᵝ̆ ͈ ̟ ̠ ̥ ̊ ̃ ̞ˀ˕̹]")
+# the above has too many spaces
+re_diacritics = re.compile(r"[ːᵝ͈̟̠̥̞̆̊̃ˀ˕̹]")
 
 with open(args.input_file) as inf, open(args.output_file, 'w') as ouf:
     for line in tqdm(inf):
@@ -20,4 +22,3 @@ with open(args.input_file) as inf, open(args.output_file, 'w') as ouf:
             grapheme = hangul_jamo.decompose(grapheme)
 
         ouf.write(f"{grapheme}\t{phoneme}\n")
-
